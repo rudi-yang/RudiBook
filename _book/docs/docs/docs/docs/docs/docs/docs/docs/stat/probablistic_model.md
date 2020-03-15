54a42e65907756f7bed890a95e4d1052bf86bf7d
rudi <rudi@rudideMBP.lan>
2020-03-16 01:35:28 +0800

    [developing]

--- _book/docs/docs/docs/docs/docs/docs/docs/docs/stat/probablistic_model.md
+++
@@ -1,3 +0,0 @@
-# 概率图模型
-
->>https://edward-cn.readthedocs.io/zh/latest/Tutorials/tutorials/#_1
--- docs/docs/docs/docs/docs/stat/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-if __name__ == '__main__':
-    d = np.linspace(0, 1, 1000)
-    print(d)
